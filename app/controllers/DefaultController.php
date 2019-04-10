b7bce24f9af7ad34f48721e31877d717954d2b68
tona-lc <nvtoan@lockon-vn.com>
2019-04-10 09:47:10 +0700

    Merge branch 'master' of https://github.com/vantoantg/mvc_basic
    # Conflicts:
    #	composer.json
    #	composer.lock

--- app/controllers/DefaultController.php
+++ app/controllers/DefaultController.php
@@ -15,15 +15,12 @@ class DefaultController extends ApplicationController
         $this->view->settings->url = '';
         if ($this->request->getMethod() == 'POST'){
             $original_url = $this->request->get('original_url');
-            if ($original_url){
+            if ($original_url && strlen($original_url) <= 500){
                 if (filter_var($original_url, FILTER_VALIDATE_URL)) {
                     $code = Helper::getInstance()->generateRandomString(5);
                     $url = new Urls();
                     $check = $url->getCodeByUrl($original_url);
                     if (!$check){
-//                        $str = addslashes('twinkle loves "coding"');
-//                        echo($str);
-//                        die;
                         $url->saveData($code, $this->request->get('original_url'));
                     }else{
                         $code = $check->code;
